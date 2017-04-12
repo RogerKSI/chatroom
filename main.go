ecc58ec1afc6af56e4a2466526a0ed7c5bbd05f1
Korrawe Karunratanakul <korrawe@gmail.com>
2017-04-13 01:30:47 +0700

    Handle home and chatroom seperately

--- main.go
+++ main.go
@@ -10,12 +10,18 @@ import (
 
 var addr = flag.String("addr", ":8080", "http service address")
 
-var validPath = regexp.MustCompile("^(/ws)?/([0-9]+)$")
+var validPath = regexp.MustCompile("^/ws/([0-9]+)$")
+var validRoomPath = regexp.MustCompile("^/room/([0-9]+)$")
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL)
-	m := validPath.FindStringSubmatch(r.URL.Path)
+	/*m := validPath.FindStringSubmatch(r.URL.Path)
 	if m == nil {
+		http.Error(w, "Not found", 404)
+		return
+	}*/
+	
+	if r.URL.Path != "/" {
 		http.Error(w, "Not found", 404)
 		return
 	}
@@ -27,6 +33,21 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "home.html")
 }
 
+func serveRoom(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	m := validRoomPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.Error(w, "Not found", 404)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", 405)
+		return
+	}
+	http.ServeFile(w, r, "chatroom.html")
+}
+
 func hubHandler(w http.ResponseWriter, r *http.Request) {
 	// log.Println(r.URL.Path)
 	m := validPath.FindStringSubmatch(r.URL.Path)
@@ -34,7 +55,7 @@ func hubHandler(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	id := m[2]
+	id := m[1]
 
 	hub := getHub(id)
 
@@ -54,6 +75,7 @@ var UserLog = SafeUserLog{v: make(map[UserKey]int)}
 func main() {
 	flag.Parse()
 	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/room/", serveRoom)
 	http.HandleFunc("/ws/", hubHandler)
 
 	fs := http.FileServer(http.Dir("resources"))
